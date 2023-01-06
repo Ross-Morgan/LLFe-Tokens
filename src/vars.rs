154b584e5224182aad7dc9b8b5e0e363c3f9b56c
Ross Morgan <morgan.ross.512@gmail.com>
2023-01-06 00:41:12 +0000

    add NewData trait to Var

--- src/vars.rs
+++ src/vars.rs
@@ -1,8 +1,17 @@
+use crate::traits::NewData;
+
 use super::Token;
 
 
 #[derive(Clone, Debug, Hash, PartialEq)]
 pub struct Var {
-    name: String,
-    value: Token,
+    pub(super) name: String,
+    pub(super) value: Token,
+}
+
+
+impl NewData for Var {
+    fn new(name: String, value: Token) -> Self {
+        Self { name, value }
+    }
 }
