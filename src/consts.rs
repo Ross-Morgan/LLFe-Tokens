4d6bf0b85d645c0f2cb767c5cb108738f3f83078
Ross Morgan <morgan.ross.512@gmail.com>
2023-01-06 00:41:02 +0000

    add NewData trait to Const

--- src/consts.rs
+++ src/consts.rs
@@ -1,7 +1,16 @@
+use crate::traits::NewData;
+
 use super::Token;
 
 #[derive(Clone, Debug, Hash, PartialEq)]
 pub struct Const {
-    name: String,
-    value: Token,
+    pub(super) name: String,
+    pub(super) value: Token,
+}
+
+
+impl NewData for Const {
+    fn new(name: String, value: Token) -> Self {
+        Self { name, value }
+    }
 }
